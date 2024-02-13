eb4f07887fe44f638f71678d596f9f041470aa84
Guillaume Tomasi <guillaume.tomasi.76@gmail.com>
2024-02-13 19:05:39 +0100

    Autocompletion ajout ingredient

--- Frontend/js/new-recipe-page.js
+++ Frontend/js/new-recipe-page.js
@@ -2,16 +2,56 @@ const blocAddIngredients = document.querySelector('.bloc-add-ingredients');
 const addIngredientPage = document.querySelector('#add-ingredient-page');
 const blocAddIngredientPage = document.querySelector('#bloc-add-ingredient-page');
 const quitIcon = document.querySelector('#quit-icon');
+const inputNameIngredient = document.querySelector('#ingredient');
 
 
+// let allIngredients = [];
+
+const getIngredients = async () => {
+   await fetch('http://localhost:3000/api/ingredient')
+      .then(res => res.json())
+      .then(data => {
+         const ingredients = data.ingredients.map(ingredient => {
+            return {
+               name: ingredient.name,
+               image: ingredient.image,
+
+            }
+         });
+         for (let i = 0; i < ingredients.length; i++) {
+            // console.log(ingredients[i].image);
+            let suggestionList = `<ul id="suggestions">
+   <li class="ingredient-suggestion">
+      <div class="img-ingredient-suggestion">
+         <img
+            src="${ingredients[i].image}"
+            alt="${ingredients[i].name}"
+         />
+      </div>
+      <div class="name-ingredient-suggestion">${ingredients[i].name}</div>
+   </li>
+</ul>`;
+
+            document.querySelector('.add-name-ingredient').insertAdjacentHTML('beforeend', suggestionList);
+         }
+      })
+      .catch(err => console.log(err))
+};
+
+inputNameIngredient.addEventListener('focus', () => {
+   getIngredients();
+})
+
+
+
+
+
+// Ajout du bloc "Ajouter un ingrédient"
 
 blocAddIngredients.addEventListener('click', (event) => {
-
    // Afficher la page d'ajout d'ingrédients
    blocAddIngredientPage.style.display = "block";
    addIngredientPage.style.display = "block";
-
-
 
    // Désactiver les interactions pour tous les autres éléments
    document.body.style.pointerEvents = 'none';
@@ -24,10 +64,11 @@ blocAddIngredients.addEventListener('click', (event) => {
 
    setTimeout(() => { // Utilisation d'un délai pour déclencher la transition après le changement de display
       addIngredientPage.style.left = "0";
-      // window.getComputedStyle(addIngredientPage).opacity;
    }, 10);
 
 });
+
+// Suppression du bloc "Ajouter un ingrédient"
 
 quitIcon.addEventListener('click', () => {
 
