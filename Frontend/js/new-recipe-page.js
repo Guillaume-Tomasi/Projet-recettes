111da4b7b3df8ca8cba362244d238e619e782653
Guillaume Tomasi <guillaume.tomasi.76@gmail.com>
2024-03-07 16:41:33 +0100

    ajout d'étapes

--- Frontend/js/new-recipe-page.js
+++ Frontend/js/new-recipe-page.js
@@ -370,11 +370,165 @@ submitNewIngredients.addEventListener('click', () => {
 
 
 
+
 // Etapes
 
 const blocAddSteps = document.querySelector('.bloc-add-steps');
 const blocAddStepPage = document.querySelector('#bloc-add-step-page');
 const addStepPage = document.querySelector('#add-step-page');
+const submitBtnStep = document.getElementById('submit-new-step');
+
+
+
+
+let addedStep = [];
+
+let blocNewStepText = `<form class="bloc-new-step">
+            <div class="add-description">
+              <h3 class="title-step">Description de l'étape ${addedStep.length + 1}</h3>
+              <span class="characters-count">0/400</span>
+              <textarea
+                name="description"
+                id="description"
+                cols="30"
+                rows="6"
+                maxlength="400"
+                placeholder="Préchauffer le four..."
+              ></textarea>
+            </div>
+            <div class="select-ingredients">
+              <h3 class="title-step">Ingrédients pour l'étape ${addedStep.length + 1}</h3>
+              <div id="ingredients-step">
+                
+              </div>
+            </div>
+
+            <button type="submit" class="add-new-step">
+              <div class="add-new-step-icon">
+                <svg
+                  class="add-icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
+                  />
+                </svg>
+              </div>
+              <span>Ajouter</span>
+            </button>
+          </form>`
+
+
+
+const updateCharacterCount = () => {
+   const descriptionTextarea = document.getElementById('description');
+   const charactersCountSpan = document.querySelector('.characters-count');
+   const currentCount = descriptionTextarea.value.length;
+   charactersCountSpan.textContent = `${currentCount}/400`;
+};
+
+const updateIngredientsForSteps = () => {
+   const ingredientStep = document.getElementById('ingredients-step');
+
+   ingredientStep.innerHTML = '';
+
+   let ingredientImg = null;
+
+   addedIngredients.forEach((ingredient) => {
+
+      for (let i of allIngredients) {
+         if (i.name === ingredient.name) {
+            ingredientImg = i.image;
+         }
+      }
+
+      let ingredientsForSteps = `<div class="select-ingredient-step">
+                  <label for="${ingredient.name}">
+                    <input type="checkbox" name="${ingredient.name}" id="${ingredient.name}" />
+                    <span class="custom-checkbox"></span>
+                    <div class="custom-checkbox-checked">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                      >
+                        <path
+                          d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+                        />
+                      </svg>
+                    </div>
+
+                    <div class="img-ingredient-step">
+                      <img
+                        src="${ingredientImg}"
+                        alt="${ingredient.name}"
+                      />
+                    </div>
+                    ${ingredient.name}
+                  </label>
+                </div>`
+      ingredientStep.insertAdjacentHTML('beforeend', ingredientsForSteps)
+   })
+   updateCharacterCount();
+   document.getElementById('description').addEventListener('input', updateCharacterCount);
+
+}
+
+const updateSteps = () => {
+   document.querySelectorAll('.bloc-added-step').forEach(bloc => {
+      bloc.remove();
+   });
+   addedStep.forEach((step, index) => {
+      // Récupération de la description de l'étape
+      // const description = document.querySelector(`#description-step-${index}`).value.trim();
+
+      // Récupération des ingrédients cochés pour l'étape
+      // const checkedIngredients = [];
+      // document.querySelectorAll(`#ingredients-step-${index} input[type="checkbox"]:checked`).forEach(checkbox => {
+      //    checkedIngredients.push(checkbox.name);
+      // });
+
+      // Maintenant, vous pouvez utiliser 'description' et 'checkedIngredients' comme vous le souhaitez.
+      // console.log(`Étape ${index + 1}: Description - ${description}, Ingrédients cochés - ${checkedIngredients}`);
+
+      let blocStepText = `<form class="bloc-new-step bloc-added-step" id="bloc-new-step-${index}">
+            <div class="add-description">
+              <h3 class="title-step">Description de l'étape ${index + 1}</h3>
+              <span class="characters-count">0/400</span>
+              <textarea
+                name="description-step-${index}"
+                id="description-step-${index}"
+                class="description"
+                cols="30"
+                rows="6"
+                maxlength="400"
+                placeholder="Préchauffer le four..."
+               
+              >${step.description}</textarea>
+            </div>
+            <div class="select-ingredients">
+              <h3 class="title-step">Ingrédients pour l'étape ${index + 1}</h3>
+              <div id="ingredients-step-${index}" class="ingredients-step">
+                
+              </div>
+            </div>
+
+            <button type="submit" class="delete-new-step">
+              <div class="delete-new-step-icon">
+              
+                <svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
+              </div>
+              <span>Supprimer</span>
+            </button>
+          </form>`
+
+      document.getElementById('add-step-header').insertAdjacentHTML('afterend', blocStepText);
+
+   });
+}
+
+
+
 
 // Fonction pour fermer la page d'ajout d'étapes
 const closeAddStepPage = () => {
@@ -387,6 +541,9 @@ const closeAddStepPage = () => {
 };
 
 blocAddSteps.addEventListener('click', () => {
+   // document.querySelectorAll('.bloc-new-step').forEach(bloc => {
+   //    bloc.remove();
+   // })
    // Afficher la page d'ajout d'étapes
    blocAddStepPage.style.display = "block";
    addStepPage.style.display = "block";
@@ -405,4 +562,27 @@ blocAddSteps.addEventListener('click', () => {
    }, 10);
 
    document.querySelector('#add-step-header .quit-icon').addEventListener('click', closeAddStepPage);
+
+
+   submitBtnStep.insertAdjacentHTML('beforebegin', blocNewStepText);
+
+   updateIngredientsForSteps();
+
+   const addNewStepBtn = document.querySelector('.add-new-step');
+
+   addNewStepBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const newStep = {
+         description: document.querySelector(`#description`).value.trim(),
+         ingredients: []
+      };
+      document.querySelectorAll(`#ingredients-step input[type="checkbox"]:checked`).forEach(checkbox => {
+         newStep.ingredients.push(checkbox.name);
+      });
+      addedStep.push(newStep);
+      updateSteps();
+
+      console.log(addedStep);
+   })
+
 })
